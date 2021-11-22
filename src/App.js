ea1bd2716b0ce05b66bf2ebe6d379df0abfbed26
sidshetty <siddharthshetty@live.com>
2021-11-23 00:00:06 +0530

    context API integrated

--- src/App.js
+++ src/App.js
@@ -1,47 +1,21 @@
-import React, { useState, useEffect } from 'react';
+import React, { useContext } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
-import AuthContext from './components/store/auth-context';
+import AuthContext from './store/auth-context';
 
 function App() {
-	const storedUserLoggedInInfo = localStorage.getItem('isLoggedIn');
-
-	const [ isLoggedIn, setIsLoggedIn ] = useState(false);
-
-	useEffect(() => {
-		if (storedUserLoggedInInfo === '1') {
-			setIsLoggedIn(true);
-		}
-	}, []);
-
-	const loginHandler = (email, password) => {
-		// We should of course check email and password
-		// But it's just a dummy/ demo anyways
-		localStorage.setItem('isLoggedIn', '1');
-		setIsLoggedIn(true);
-	};
-
-	const logoutHandler = () => {
-		localStorage.setItem('isLoggedIn', '0');
-		setIsLoggedIn(false);
-	};
+	const ctx = useContext(AuthContext);
 
 	return (
-		<AuthContext.Provider
-			value={{
-				isLoggedIn: isLoggedIn,
-				onLogout: logoutHandler
-			}}
-		>
-			{/* all chlidren inside this have access to it. if i wrap only login with this then only login gets access to it */}
+		<React.Fragment>
 			<MainHeader />
 			<main>
-				{!isLoggedIn && <Login onLogin={loginHandler} />}
-				{isLoggedIn && <Home onLogout={logoutHandler} />}
+				{!ctx.isLoggedIn && <Login />}
+				{ctx.isLoggedIn && <Home />}
 			</main>
-		</AuthContext.Provider>
+		</React.Fragment>
 	);
 }
 
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect, useReducer } from 'react';
+import React, { useState, useEffect, useReducer, useContext } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
+import AuthContext from '../../store/auth-context';
 
 const emailReducer = (state, action) => {
 	if (action.type === 'USER_INPUT') {
@@ -74,10 +75,10 @@ const Login = (props) => {
 		//setPasswordIsValid(passwordState.value.trim().length > 6);
 		dispatchPassword({ type: 'INPUT_BLUR' });
 	};
-
+	const authctx = useContext(AuthContext);
 	const submitHandler = (event) => {
 		event.preventDefault();
-		props.onLogin(emailState.value, passwordState.value);
+		authctx.onLogin(emailState.value, passwordState.value);
 	};
 
 	return (
